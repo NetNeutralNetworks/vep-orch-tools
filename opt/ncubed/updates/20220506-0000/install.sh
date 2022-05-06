ed9387d727a5a7cc6db3958b4137f218c994e95d
Piet Braat <piet.braat@matrixmind.nl>
2022-05-06 17:32:23 +0200

    -

--- opt/ncubed/updates/20220506-0000/install.sh
+++ opt/ncubed/updates/20220506-0000/install.sh
@@ -8,9 +8,9 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 rm /opt/ncubed/*
 rm -r /opt/ncubed/*.service
-cp -r $SCRIPT_DIR/ncubed/* /opt/ncubed
+cp -p -r $SCRIPT_DIR/ncubed/* /opt/ncubed
 systemctl daemon-reload
 chmod +x /opt/ncubed/oneshot.service/install
 /opt/ncubed/oneshot.service/install
 
-cp -r /opt/ncubed/etc/* /etc/
+cp -p -r /opt/ncubed/etc/* /etc/
