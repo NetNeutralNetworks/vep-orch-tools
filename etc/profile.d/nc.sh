2c120c149a34bc7b6c1f658a9e140dcd793deba0
Piet Braat <piet.braat@matrixmind.nl>
2022-08-02 16:18:29 +0200

    update nc.sh

--- etc/profile.d/nc.sh
+++ etc/profile.d/nc.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 export ANSIBLE_INVENTORY=/opt/ncubed/ansible/inventories
 
+# this will create a tmux session and will reattatch if it allready exists
 [ -z "$TMUX"  ] && { tmux attach || exec tmux new-session && exit; }
+
+print_service_state () {
+  state=$(systemctl is-active $1)
+  case $state in
+    active)
+      color="\e[32m"
+    ;;
+    *)
+      color="\e[31m"
+    ;;
+  esac
+  printf "\e[1m$1: $color\t$state\e[0m\n"
+}
 
 printf "
 To show dynamic motd run: cat /var/run/motd.dynamic
@@ -10,23 +24,9 @@ ansible inventory: /opt/ncubed/ansible/inventories
 the inventory and ssh known hosts are automaticly updated by the attestation sync service
 "
 
+printf "\n"
+print_service_state "ncubed-attestation-sync"
 
-state=$(systemctl is-active ncubed-attestation-sync)
-case $state in
-active)
-printf "\e[32m
-ncubed-attestation-sync: $(systemctl is-active ncubed-attestation-sync)
-\e[0m
-"
-;;
-*)
-printf "\e[31m
-ncubed-attestation-sync: $(systemctl is-active ncubed-attestation-sync)
-\e[0m
-"
-;;
-esac
-#systemctl status ncubed-attestation-sync.service
 if [[ -z "${ANSIBLE_USER}" ]]; then
   printf "\e[31m
         ANSIBLE_USER not set
