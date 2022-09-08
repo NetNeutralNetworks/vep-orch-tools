87ed892e3aa28f20945b3012f232f38891fa467e
Piet Braat <piet.braat@matrixmind.nl>
2022-09-08 11:11:14 +0200

    added auto ip subnet and config

--- opt/ncubed/updates/20220830-0000/ncubed/network.service/detect_subnet.py
+++ opt/ncubed/updates/20220830-0000/ncubed/network.service/detect_subnet.py
@@ -1,6 +1,12 @@
 import subprocess
 import concurrent.futures
+from functools import partial
 import ipaddress
+import yaml
+
+import sys
+import logging
+from logging.handlers import RotatingFileHandler
 
 '''
 This script will try to detect the network and the gateway for a specific WAN interface
@@ -10,17 +16,26 @@ After a network is establisched al ip adresses found are tried as gateway for a 
 '''
 
 MAX_WORKERS=500
+GW_TEST_IP="8.8.8.8"
+
+# logger = logging.getLogger("ncubed nework daemon")
+# logger.setLevel(level=logging.DEBUG)
+# formatter = logging.Formatter(fmt='%(asctime)s(File:%(name)s,Line:%(lineno)d, %(funcName)s) - %(levelname)s - %(message)s', 
+#                                 datefmt="%m/%d/%Y %H:%M:%S %p")
+# rothandler = RotatingFileHandler('/var/log/ncubed.neworkd.log', maxBytes=100000, backupCount=5)
+# rothandler.setFormatter(formatter)
+# logger.addHandler(rothandler)
 
 def exec_pool(FUNCTION,LIST):
     with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
         executor.map(FUNCTION,LIST)
 
-def ping_host(IP):
+def ping_host(WANINTF,IP):
     subprocess.run(f'''
         ip netns exec ns_{WANINTF} ping -c 1 -W 1 {IP}
         ''', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-def find_gateway(detected_ips):
+def find_gateway(WANINTF,detected_ips):
     while detected_ips:
         TEST_IP = detected_ips.pop(0)
         print(f"Trying {TEST_IP} as gateway")
@@ -28,15 +43,18 @@ def find_gateway(detected_ips):
             ip netns exec ns_{WANINTF} ip route del 0.0.0.0/0
             ip netns exec ns_{WANINTF} ip route add 0.0.0.0/0 via {TEST_IP}
             ''', shell=True)
-        output = subprocess.run(f"ip netns exec ns_{WANINTF} ping -c 1 -W 1 8.8.8.8",stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).stdout.decode()
+        output = subprocess.run(f"ip netns exec ns_{WANINTF} ping -c 1 -W 1 {GW_TEST_IP}",stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).stdout.decode()
         if "received, 0% packet loss" in output:
             print(f"Using {TEST_IP} as gateway")
-            return 0
+            return TEST_IP
 
     print(f"No usable gateway found!!!")  
-    return 1
+    return ''
 
 def test_subnet(WANINTF,reference_ip):
+    '''
+    try continuously increasingly large subnets and check for available ip addresses, if adresses are available, pick last ip.
+    '''
     for CIDR in range(30,23, -1):
         PROBABLE_NETWORK = ipaddress.IPv4Interface(f"{reference_ip}/{CIDR}").network
         subprocess.run(f'''
@@ -45,23 +63,27 @@ def test_subnet(WANINTF,reference_ip):
             ip netns exec ns_{WANINTF} ip -br -c addr show dev br-{WANINTF}_e
             ''', shell=True)
 
-        exec_pool(ping_host, list(WANINTF,PROBABLE_NETWORK))
+        exec_pool(partial(ping_host, WANINTF), list(PROBABLE_NETWORK))
         detected_ips = subprocess.run(f"ip netns exec ns_{WANINTF} ip neigh | grep lladdr | cut -d ' ' -f1", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).stdout.decode().split()
         if len(detected_ips) < PROBABLE_NETWORK.num_addresses-2:
             available_ips = sorted(set([str(address) for address in list(PROBABLE_NETWORK)[1:-1]])-set(detected_ips))
+            last_ip=f"{available_ips[-1]}/{CIDR}"
             subprocess.run(f'''
                 ip netns exec ns_{WANINTF} ip addr flush dev br-{WANINTF}_e
-                ip netns exec ns_{WANINTF} ip addr add {available_ips[-1]}/{CIDR} dev br-{WANINTF}_e
+                ip netns exec ns_{WANINTF} ip addr add {last_ip} dev br-{WANINTF}_e
                 ip netns exec ns_{WANINTF} ip -br -c addr show dev br-{WANINTF}_e
                 ''', shell=True)
-            if find_gateway(detected_ips) == 0:
-                return 0
+            gateway = find_gateway(WANINTF,detected_ips)
+            if gateway:
+                # exit if valid gateway is found
+                return {'ip':last_ip,'gateway':gateway}
+    return {}
 
 def capture_reference_ip(WANINTF):
     # using Popen to be able to continuously monitor tcpdump output
     with subprocess.Popen(f"ip netns exec ns_{WANINTF} tcpdump -i br-{WANINTF}_e -U -l", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, universal_newlines=True) as p:
         for line in p.stdout:
-            print(line, end='')
+            #print(line, end='')
             if len(line.split(',')) > 1:
                 words=line.split(',')[1].split()
                 if len(words) > 4:
@@ -69,10 +91,22 @@ def capture_reference_ip(WANINTF):
                         p.terminate()
                         return words[2]
 
+def save_config(WANINTF,settings):
+    FILENAME = f'/opt/ncubed/config/{WANINTF}.yaml'
+
+
+    with open(FILENAME,'w+') as wfile:
+        with open(FILENAME,'r') as file:
+            data = yaml.load(file, Loader=yaml.FullLoader)
+            if not data:
+                data = {}
+            data.update({'settings':settings})
+        yaml.dump(data, wfile, sort_keys=True)
+
 def configure_wan_interface(WANINTF):
     reference_ip = capture_reference_ip(WANINTF)
-    detected_ips = test_subnet(WANINTF,reference_ip)
-    find_gateway(WANINTF,detected_ips)
+    settings = test_subnet(WANINTF,reference_ip)
+    save_config(WANINTF,settings)
 
 if __name__ == "__main__":
     configure_wan_interface('WAN0')
--- opt/ncubed/updates/20220830-0000/ncubed/network.service/init.py
+++ opt/ncubed/updates/20220830-0000/ncubed/network.service/init.py
@@ -7,20 +7,21 @@ import subprocess
 import logging
 from logging.handlers import RotatingFileHandler
 
-#logging.basicConfig(filename='/var/log/ncubed.neworkd.log', level=logging.DEBUG)
-# logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
-#                     level=logging.DEBUG,
-#                     datefmt='%Y-%m-%d %H:%M:%S')
-logger = logging.getLogger("ncubed network daemon")
+logger = logging.getLogger("ncubed nework daemon")
 logger.setLevel(level=logging.DEBUG)
 formatter = logging.Formatter(fmt='%(asctime)s(File:%(name)s,Line:%(lineno)d, %(funcName)s) - %(levelname)s - %(message)s', 
                                 datefmt="%m/%d/%Y %H:%M:%S %p")
-rothandler = RotatingFileHandler('/var/log/ncubed.networkd.log', maxBytes=100000, backupCount=5)
+rothandler = RotatingFileHandler('/var/log/ncubed.neworkd.log', maxBytes=100000, backupCount=5)
 rothandler.setFormatter(formatter)
 logger.addHandler(rothandler)
 logger.addHandler(logging.StreamHandler(sys.stdout))
 
 ifdata = json.loads(subprocess.Popen(f"ip -j addr", stdout=subprocess.PIPE, shell=True).stdout.read())
+
+import detect_subnet
+def check_ip_configured(WANINTF):
+    ip_configured = subprocess.run(f'''ip -4 -n ns_{WANINTF} -br addr show dev br-{WANINTF}_e''', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).stdout.decode()
+    return ip_configured
 
 def create_trunkports(BONDNAME, INTERFACES, BRIDGENAME):
     logger.info(f"Creating {BONDNAME}")
@@ -38,12 +39,12 @@ def create_trunkports(BONDNAME, INTERFACES, BRIDGENAME):
         ''', stdout=subprocess.PIPE, shell=True)
 
     subprocess.call(f'''
-        ip link add {BRIDGENAME} type bridge vlan_filtering 1
-        ip link set dev {BONDNAME} master {BRIDGENAME}
-        bridge vlan add dev {BRIDGENAME} vid 1-4094 self
-        bridge vlan add dev {BONDNAME} vid 1-4094
-        bridge vlan add dev {BRIDGENAME} vid 1 pvid self untagged
-        bridge vlan add dev {BONDNAME} vid 1 pvid untagged
+    ip link add {BRIDGENAME} type bridge vlan_filtering 1
+    bridge vlan del dev {BRIDGENAME} vid 1 self
+    bridge vlan del dev {BONDNAME} vid 1
+    ip link set dev {BONDNAME} master {BRIDGENAME}
+    bridge vlan add dev {BRIDGENAME} vid 3100-3299 self
+    bridge vlan add dev {BONDNAME} vid 3100-3299
     ''', stdout=subprocess.PIPE, shell=True)
 
     subprocess.call(f'''
@@ -114,7 +115,7 @@ def create_wanport (ID, INTF, TRUNKBRIGE, TRANSIT_PREFIX=None):
     ip link set {VETH_L2_E} netns {NETNS}
     ''', shell=True)
     
-    logger.info(f"Configure namespace")
+    logger.info(f"Configure netns {NETNS}")
     subprocess.call(f'''
     ip netns exec {NETNS} ip link add {BRIDGE_E} type bridge
     ip netns exec {NETNS} ip link set {VETH_L2_E} master {BRIDGE_E}
@@ -164,13 +165,26 @@ def create_wanport (ID, INTF, TRUNKBRIGE, TRANSIT_PREFIX=None):
             ip netns exec {NETNS} dhclient {BRIDGE_E} &
             ''', shell=True)
     else:
-        # Falling back to DHCP request
-        subprocess.call(f'''
-        ip netns exec {NETNS} dhclient {BRIDGE_E} &
-        ''', shell=True)
+        WANINTF = DOMAIN
+        while not check_ip_configured(WANINTF):
+            if not 'up' in subprocess.run(f'''ip -n ns_{WANINTF} -br addr show dev {EXTERNAL_NIC}''', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).stdout.decode().lower():
+                time.sleep(5)
+                continue
 
-        # If all else fails set manually via fancy ARP script
-        # TODO: Implement fancy arp script
+            logger.info(f'Trying DHCP on br-{WANINTF}_e')
+            with subprocess.Popen(f"ip netns exec ns_{WANINTF} dhclient br-{WANINTF}_e -d", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, universal_newlines=True) as p:
+                lines = []
+                for line in p.stdout:
+                    lines.append(line)
+                    #print(line, end='')
+                    if len([line for line in lines if 'DHCPDISCOVER' in line]) >= 3 or 'DHCPACK' in line:
+                        time.sleep(0.2)
+                        p.terminate()
+                        break
+            
+            if not check_ip_configured(WANINTF):
+                logger.info(f'no DHCP detected trying auto detect script on br-{WANINTF}_e')
+                detect_subnet.configure_wan_interface(WANINTF)
 
     logger.info(subprocess.run(f''' printf "\n\n"
     ip netns exec {NETNS} ip -br addr
@@ -198,6 +212,11 @@ server=9.9.9.9
     ip netns exec {NETNS} /usr/sbin/dnsmasq --conf-file=/opt/ncubed/network.service/dnsmasq/{DOMAIN}.conf
     ''', shell=True)
 
+    # logger.info(f"Add bridges to correct firewalld zone")
+    # subprocess.Popen(f'''
+    # firewall-cmd --zone=trusted --change-interface={BRIDGE_I}
+    # firewall-cmd --zone=trusted --change-interface={BRIDGE_I} --permanent
+    # ''', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 
     # logger.info(f"Reattaching vnet interfaces")
     # CURRENT_BRIDGELINKS=[L for L in json.loads(subprocess.run(f"bridge -j link", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).stdout) if L.get('master') in [BRIDGE_I, BRIDGE_L2_I]]
@@ -222,29 +241,14 @@ if __name__ == '__main__':
             create_trunkports(BOND['name'], BOND['interfaces'], BOND['bridgename'])
 
         BRIDGENAME = DEV_CONFIG[0]['portconfig']['INT'][0]
+
+        from threading import Thread
+        threads = []
         for k,v in DEV_CONFIG[0]['portconfig']['WAN'].items():
-            create_wanport(k,v, BRIDGENAME)
-
-    if os.path.exists(f'/opt/ncubed/config/vlan_bridges.yaml'):
-        logger.info(f"found config for adding vlan bridges")
-        with open(f'/opt/ncubed/config/vlan_bridges.yaml') as f:
-            logger.info(f"Adding vlan bridges")
-            vlan_bridges = yaml.load(f, Loader=yaml.FullLoader)
-            for k, v in vlan_bridges.items():
-                for vlan in v:
-                    logger.info(f"Creating vlan bridge: {vlan} on trunk: {k}")
-                    subprocess.run(f'''
-                    ip link add name br-{vlan} type bridge
-                    sudo ip link add link {k} name {k}.{vlan} type vlan id {vlan}
-                    sudo ip link set up dev {k}.{vlan}
-                    sudo ip link set {k}.{vlan} master br-{vlan}
-                    sudo ip link set up dev br-{vlan}
-                    ''', shell=True)
-
-    try:
-        subprocess.run(f"python3 /etc/opt/ncubed/custom_network.py", shell=True)
-    except Exception as e:
-        logger.warning(f"Error running custom network script: {e}")
+            thread = Thread(target=create_wanport, args=(k,v, BRIDGENAME))
+            thread.start()
+            threads.append(thread)
+            time.sleep(1)
 
     while True:
         time.sleep(5)
