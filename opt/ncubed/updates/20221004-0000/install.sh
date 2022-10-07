868e8e364b4bb02e1d613e39d51c38c3398d6a82
Piet Braat <piet.braat@matrixmind.nl>
2022-10-07 17:42:50 +0200

    fix STP

--- opt/ncubed/updates/20221004-0000/install.sh
+++ opt/ncubed/updates/20221004-0000/install.sh
@@ -28,14 +28,18 @@ cp  $ROOTDIR/config/netplan-config.yaml /etc/netplan/config.yaml
 netplan apply
 systemctl restart network-manager.service
 
+# update etc files
+cp -p -r $ROOTDIR/etc/* /etc/
+
+# make sure bin dir is executiable
+chmod +x $ROOTDIR/bin/*
+
+# add cli to globally available tools
+ln $ROOTDIR/bin/n3.py /usr/local/bin/n3
+
 # install services
 chmod +x $ROOTDIR/network.service/install
 $ROOTDIR/network.service/install
 
 chmod +x $ROOTDIR/callhome.service/install
 $ROOTDIR/callhome.service/install
-
-cp -p -r $ROOTDIR/etc/* /etc/
-
-# add cli to globally available tools
-ln $ROOTDIR/bin/n3.py /usr/local/bin/n3
