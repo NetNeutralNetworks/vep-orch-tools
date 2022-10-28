312574d58964b4b45244d704f17458e655c50b10
Piet Braat <piet.braat@matrixmind.nl>
2022-10-28 14:57:24 +0200

    fix snmp

---
+++ opt/ncubed/updates/00000000-dev/install.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+##################################################################################
+# elevate
+##################################################################################
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+ROOTDIR="/opt/ncubed"
+
+# cleanup original setup
+systemctl disable firewalld
+systemctl stop firewalld
+systemctl mask firewalld
+apt remove firewalld
+
+mkdir -p $ROOTDIR
+rm $ROOTDIR/*
+rm -r $ROOTDIR/*.service
+
+########################################################
+# install update
+########################################################
+cp -p -r $SCRIPT_DIR/ncubed/* $ROOTDIR
+
+# clean netplan
+rm /etc/netplan/*
+cp  $ROOTDIR/config/netplan-config.yaml /etc/netplan/config.yaml
+netplan apply
+systemctl restart network-manager.service
+
+# update etc files
+cp -p -r $ROOTDIR/etc/* /etc/
+
+# make sure bin dir is executiable
+chmod +x $ROOTDIR/bin/*
+
+# add cli to globally available tools
+ln $ROOTDIR/bin/n3.py /usr/local/bin/n3
+
+# install services
+chmod +x $ROOTDIR/network.service/install
+$ROOTDIR/network.service/install
+
+chmod +x $ROOTDIR/callhome.service/install
+$ROOTDIR/callhome.service/install
+
+chmod +x /etc/libvirt/hooks/qemu
+systemctl restart libvirtd
+systemctl restart docker
+touch /var/log/ncubed.libvirt.log
+
+# FIX: sudo unable to reolve hostname
+echo ::1 $(hostname) >> /etc/hosts
+
+# just a hack
+printf -- "---
+platform: $(sudo /usr/sbin/dmidecode -s system-family)
+serial: $(sudo /usr/sbin/dmidecode -s system-serial-number)
+" > /opt/ncubed/system.yaml
