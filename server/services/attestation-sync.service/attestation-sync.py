51f7f47babafb03d9911881f748f77d11d7e3edb
Lab - dev (Jasper) <beheer@ncubed.nl>
2023-12-21 16:04:33 +0000

    Fixed crashing code

--- server/services/attestation-sync.service/attestation-sync.py
+++ server/services/attestation-sync.service/attestation-sync.py
@@ -218,20 +218,20 @@ while True:
 
     results = json.loads(r.text)
     
-    if 'results' in results
+    if 'results' in results:
         for result in results['results']:
-        IPV6 = "{}{}/128".format(config.get('IPV6_SUPERNET').split('/')[0], result['device_id'])
-        IPV4 = "{}.{}.{}/32".format(config.get('IPV4_PREFIX'), result['device_id'] >> 8 & 255, result['device_id'] & 255)
-        if not result['device_id']:
-            continue
-        if result['approved']:
-            os.system("wg set wg0 peer {} allowed-ips {},{}".format(result['client_pub_key'], IPV4, IPV6))
-            update_ansible(IPV6.split('/')[0],"add", {'wg_public_key': result['client_pub_key'], 'allowed_ips': [IPV4, IPV6]})
-            logger.info(f"Added {IPV6.split('/')[0]} to inventory")
-        else:
-            os.system("wg set wg0 peer {} remove".format(result['client_pub_key']))
-            update_ansible(IPV6.split('/')[0],"remove")
-            logger.info(f"Removed {IPV6.split('/')[0]} from inventory")
+            IPV6 = "{}{}/128".format(config.get('IPV6_SUPERNET').split('/')[0], result['device_id'])
+            IPV4 = "{}.{}.{}/32".format(config.get('IPV4_PREFIX'), result['device_id'] >> 8 & 255, result['device_id'] & 255)
+            if not result['device_id']:
+                continue
+            if result['approved']:
+                os.system("wg set wg0 peer {} allowed-ips {},{}".format(result['client_pub_key'], IPV4, IPV6))
+                update_ansible(IPV6.split('/')[0],"add", {'wg_public_key': result['client_pub_key'], 'allowed_ips': [IPV4, IPV6]})
+                logger.info(f"Added {IPV6.split('/')[0]} to inventory")
+            else:
+                os.system("wg set wg0 peer {} remove".format(result['client_pub_key']))
+                update_ansible(IPV6.split('/')[0],"remove")
+                logger.info(f"Removed {IPV6.split('/')[0]} from inventory")
     else:
         logger.debug(results)
 
