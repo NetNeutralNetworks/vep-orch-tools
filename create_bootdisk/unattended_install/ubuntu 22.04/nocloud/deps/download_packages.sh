3ad19dfe522915ad3555b6f2d7187e6eee4e1805
Piet Braat <piet.braat@ncubed.nl>
2024-01-16 17:11:04 +0100

    UPD: easy download packages

--- create_bootdisk/unattended_install/ubuntu 22.04/nocloud/deps/download_packages.sh
+++ create_bootdisk/unattended_install/ubuntu 22.04/nocloud/deps/download_packages.sh
@@ -1,14 +1,41 @@
 # get dependencyies
 # run this script from this directory to download debs
+ORIGPATH=$(pwd)
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+echo $SCRIPTPATH
+cd "$SCRIPTPATH"
 
-for i in $(apt-cache depends i2c-tools | grep -E 'Depends' | cut -d ':' -f 2,3 | sed -e s/'<'/''/ -e s/'>'/''/); do apt-get download $i; done
-apt-get download i2c-tools
+printf "
+$(pwd)
+"
 
-for i in $(apt-cache depends efibootmgr | grep -E 'Depends' | cut -d ':' -f 2,3 | sed -e s/'<'/''/ -e s/'>'/''/); do apt-get download $i; done
-apt-get download efibootmgr
+rm *.deb
+
+options="-o=dir::cache=./ -o=dir::etc::sourcelist=../../sources.list"
+sudo apt update $options
+
+PACKAGES=(
+"i2c-tools"
+"efibootmgr"
+)
+
+for package in ${PACKAGES[*]}
+do
+    printf "
+    #####################################
+    $package
+    #####################################
+    "
+    apt-get download $options $(apt-cache depends $options $package | grep -E 'Depends' | cut -d ':' -f 2,3 | sed -e s/'<'/''/ -e s/'>'/''/)
+    apt-get download $options i2c-tools
+done
 
 #for i in $(apt-cache depends jq | grep -E 'Depends|Recommends|Suggests' | cut -d ':' -f 2,3 | sed -e s/'<'/''/ -e s/'>'/''/); do apt-get download $i; done
 #apt-get download jq
 
 # FIX: libc6 will not install, workarround remove deb (no effect on installable)
 rm libc6*
+rm -f *.bin
+
+cd "$ORIGPATH"
