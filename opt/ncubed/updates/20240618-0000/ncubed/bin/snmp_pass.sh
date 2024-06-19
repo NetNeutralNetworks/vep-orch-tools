fa34af6ea4c9e3a0f1b4067286dea5770435a571
Piet Braat <piet.braat@ncubed.nl>
2024-06-19 23:32:15 +0200

    ADD: snmp updates table

--- opt/ncubed/updates/20240618-0000/ncubed/bin/snmp_pass.sh
+++ opt/ncubed/updates/20240618-0000/ncubed/bin/snmp_pass.sh
@@ -1,11 +1,10 @@
 #!/bin/sh -f
-
 PLACE=".1.3.6.1.4.1.61192"       # ncubed OID
 REQ="$2"                         # Requested OID
 
 # .0.x opbjectid's , not used
 # .1.x general
-# .2.<ojectid>.y object specific 
+# .2.<ojectid>.y object specific
 
 #
 #  GETNEXT requests - determine next valid instance
@@ -23,6 +22,16 @@ if [ "$1" = "-n" ]; then
     $PLACE.1.4)    RET=$PLACE.1.5 ;;
     $PLACE.1.5)    RET=$PLACE.1.6 ;;
     $PLACE.1.6)    RET=$PLACE.1.7 ;;
+    $PLACE.1.7|    \
+    $PLACE.1.8|    \
+    $PLACE.1.8.0)      RET=$PLACE.1.8.0.0 ;;
+    $PLACE.1.8.0.0)    RET=$PLACE.1.8.0.1 ;;
+    $PLACE.1.8.0.1)    RET=$PLACE.1.8.1 ;;
+    $PLACE.1.8.1)    RET=$PLACE.1.8.1.0 ;;
+    $PLACE.1.8.1.0)    RET=$PLACE.1.8.1.1 ;;
+    $PLACE.1.8.1.1)    RET=$PLACE.1.8.2 ;;
+    $PLACE.1.8.2)    RET=$PLACE.1.8.2.0 ;;
+    $PLACE.1.8.2.0)    RET=$PLACE.1.8.2.1 ;;
     *)              exit 0 ;;
   esac
 else
@@ -38,7 +47,13 @@ else
     $PLACE.1.4|         \
     $PLACE.1.5|         \
     $PLACE.1.6|         \
-    $PLACE.1.7)     RET=$REQ ;;
+    $PLACE.1.7|         \
+    $PLACE.1.8.0.0|         \
+    $PLACE.1.8.0.1|         \
+    $PLACE.1.8.1.0|         \
+    $PLACE.1.8.1.1|         \
+    $PLACE.1.8.2.0|         \
+    $PLACE.1.8.2.1)  RET=$REQ ;;
     *)              exit 0 ;;
   esac
 fi
@@ -54,11 +69,13 @@ case "$RET" in
   $PLACE.1.3)     echo "string";    grep DISTRIB_DESCRIPTION /etc/lsb-release | cut -d '"' -f2; exit 0 ;;
   $PLACE.1.4)     echo "integer";    cat /var/lib/update-notifier/updates-available | grep sec | cut -d ' ' -f1; exit 0 ;;
   $PLACE.1.5)     echo "integer";    cat /var/lib/update-notifier/updates-available | grep 'updates can be applied immediately' | cut -d ' ' -f1; exit 0 ;;
-  $PLACE.1.6)     echo "string";    if test -f /var/run/reboot-required; then cat /var/run/reboot-required; fi; exit 0 ;;
-  $PLACE.1.7)     echo "integer";    if test -f /opt/ncubed/config/local/system.yaml; then grep "member:" /opt/ncubed/config/local/system.yaml | sed s/.*:.//g; fi; exit 0 ;;
+  $PLACE.1.6)     echo "string";    if test -f /var/run/reboot-required; then echo 1;else echo 0; fi; exit 0 ;;
+  $PLACE.1.7)     echo "integer";    if test -f /opt/ncubed/config/local/system.yaml; then grep "member:" /opt/ncubed/config/local/system.yaml | sed s/.*:.//g; else echo 0;fi; exit 0 ;;
+  $PLACE.1.8.0.0)     echo "integer";    echo 0; exit 0 ;;
+  $PLACE.1.8.0.1)     echo "integer";    echo 1; exit 0 ;;
+  $PLACE.1.8.1.0)     echo "string";    echo "security updates"; exit 0 ;;
+  $PLACE.1.8.1.1)     echo "string";    echo "all updates"; exit 0 ;;
+  $PLACE.1.8.2.0)     echo "integer";    cat /var/lib/update-notifier/updates-available | grep sec | cut -d ' ' -f1; exit 0 ;;
+  $PLACE.1.8.2.1)     echo "integer";    cat /var/lib/update-notifier/updates-available | grep 'updates can be applied immediately' | cut -d ' ' -f1; exit 0 ;;
   *)              echo "string";    echo "ack... $RET $REQ"; exit 0 ;;  # Should not happen
 esac
-
-
-
-
